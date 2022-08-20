d241600af0b0b627b3e275a8cd5d1cd8384533ce
ilyas <babsyumar@gmail.com>
2022-08-20 20:10:20 +0100

    First Major Update! We have added an image to the tweet so it adds an image of the weather forecast of the place name. We also realized that some places such as: Budapest XIX. kerulet - Hungary had dots in them, so we replaced the dots in the place name hashtag

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import io
 import random
+import time
 import urllib
+import requests
 import tweepy
+from PIL import Image
 
-
-# api key and secret
 consumer_key = 'CONSUMER_KEY/API_KEY'
 consumer_secret = 'CONSUMER_SECRET/API_SECRET'
 
@@ -33,9 +35,17 @@ cityNameHashtag = "#" + cityName
 countryNameHashTagReplacement = countryNameHashTag
 countryNameHashTagReplace = countryNameHashTagReplacement.replace("'", "").replace("-", "").replace(" ", "")
 cityNameHashtagReplacement = cityNameHashtag
-cityNameHashtagReplace = cityNameHashtagReplacement.replace("'", "").replace('-', '').replace(" ", "")
+cityNameHashtagReplace = cityNameHashtagReplacement.replace("'", "").replace('-', '').replace(" ", "").replace(".", "")
+language = "en"  # can be "en" or "ru"
+place = cityName  # can be any city, place, street, or site, geocoder automatically selects location.
+timestamp = round(time.time())  # optional timestamp, can be any unix timestamp from now, to now + three days
+r = requests.get(f"https://vwapi.herokuapp.com?lang={language}&city={place}&timestamp={timestamp}")
+image = Image.open(io.BytesIO(r.content))
+image.save("twitter-weather.jpg")
 
 tweet = cityName + " - " + countryName + "\n\n" + googleMapLink + "\n\n" + countryNameHashTagReplace + " " + cityNameHashtagReplace + " #randomPlaces"
 print(tweet)
-# tweet out the tweet
-tweeter = api.update_status(status=tweet)
+
+
+media = api.media_upload('twitter-weather.jpg')
+tweeter = api.update_status(status=tweet, media_ids=[media.media_id])
--- updateTesting.py
+++ updateTesting.py
@@ -1,4 +1,8 @@
 import random
+import requests
+import time
+from PIL import Image
+import io
 import urllib.parse
 
 
@@ -17,7 +21,17 @@ countryName = splitter[1]
 countryNameHashTag = "#" + countryName.replace(" ", "")
 cityNameHashtag = "#" + cityName.replace(" ", "")
 cityNameHashtagReplacement = cityNameHashtag
-something = cityNameHashtagReplacement.replace("'", "").replace('-', '').replace(" ")
+something = cityNameHashtagReplacement.replace("'", "").replace('-', '').replace(" ", "")
+
+language = "en"  # can be "en" or "ru"
+place = cityName  # can be any city, place, street, or site, geocoder automatically selects location.
+timestamp = round(time.time())  # optional timestamp, can be any unix timestamp from now, to now + three days
+r = requests.get(f"https://vwapi.herokuapp.com?lang={language}&city={place}&timestamp={timestamp}")
+image = Image.open(io.BytesIO(r.content))
+image.show()
 
 tweet = cityName + " - " + countryName + "\n\n" + googleMapLink + "\n\n" + countryNameHashTag + " " + something + " #randomPlaces"
 print(tweet)
+
+
+
